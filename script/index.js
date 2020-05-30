abe641cfdea8640429c6c11f70d6ded8e3757cd1
Maíra <mairatabata@outlook.com>
2020-05-30 01:23:27 -0300

    refactor: change navigation function logic

--- script/index.js
+++ script/index.js
@@ -1,68 +1,50 @@
 const INITIAL_INDEX = 0;
-let currentIndex = INITIAL_INDEX;
+const HIDDEN_CLASS_NAME = 'card--hidden';
+const testimonialIds = []; 
 
-const cards = [
-  {
-    id: 1,
-    author: 'Tanya Sinclair',
-    role: 'UX Engineer',
-    image: 'images/image-tanya.jpg',
-    imageSubtitle: "Tanya",
-    testimonial: 'I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future.'
-  },
-  {
-    id: 2,
-    author: 'John Tarkpor',
-    role: 'Junior Front-end Developer',
-    image: 'images/image-john.jpg',
-    imageSubtitle: "John",
-    testimonial: 'If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.'
-  }
-];
+let currentTestimonial = INITIAL_INDEX;
 
 const createCardId = (id) => `card-${id}`;
 
-const formatTestimonial = testimonial => `“ ${testimonial} ”`;
+const updateElementsDisplay = (currentIndex, previousIndex) => {
+  const previousElement = document.getElementById(createCardId(previousIndex));
+  previousElement.classList.add(HIDDEN_CLASS_NAME);
 
-const updateTestimonial = () => {
-  const card = cards[currentIndex];
-
-  document.querySelector('.card').id = createCardId(card.id);
-  document.getElementById('testimonial').textContent = formatTestimonial(card.testimonial);
-  document.getElementById('author').textContent = card.author;
-  document.getElementById('role').textContent = card.role;
-
-  const picture = document.getElementById('picture');
-  picture.src = card.image;
-  picture.alt = card.imageSubtitle;
+  const currentElement = document.getElementById(createCardId(currentIndex));
+  currentElement.classList.remove(HIDDEN_CLASS_NAME);
 }
 
-const changePage = (nextPage, fallback = INITIAL_INDEX) => {
-  if(cards[nextPage]) {
-    currentIndex = nextPage;
+const changeCardPage = (nextIndex, fallbackIndex = INITIAL_INDEX) => {
+  const previousIndex = currentTestimonial;
+
+  if(testimonialIds[nextIndex]) {
+    currentTestimonial = nextIndex;
   } else {
-    currentIndex = fallback;
+    currentTestimonial = fallbackIndex;
   }
 
-  updateTestimonial();
+  updateElementsDisplay(currentTestimonial, previousIndex);
 }
 
 const goToNextCard = () => {
-  const nextPage = currentIndex + 1;
+  const nextIndex = currentTestimonial + 1;
 
-  changePage(nextPage);
+  changeCardPage(nextIndex);
 }
 
 const goToPreviousCard = () => {
-  const prevPage = currentIndex - 1;
-  const fallback = cards.length -1;
+  const prevIndex = currentTestimonial - 1;
+  const fallbackIndex = testimonialIds.length - 1;
 
-  changePage(prevPage, fallback);
+  changeCardPage(prevIndex, fallbackIndex);
 }
 
-const onLoadPage = () => {
-  updateTestimonial();
-  document.querySelector('.slider').classList.remove('slider--hidden');
+const onLoadCard = () => {
+  const elements = document.querySelectorAll('.card');
+
+  [...elements].forEach(({ id }) => {
+    testimonialIds.push(id)
+  });
 }
 
-window.addEventListener('load', onLoadPage);
+window.addEventListener('load', onLoadCard);
